b35724e4c5c45a71931fc5e6c9b6a58274b14adf
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-06 07:36:58 +0900

    入力値チェック用アノテーションを追加しました。

--- src/main/java/jp/co/sample/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package jp.co.sample.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 /**
  * 管理者情報Insert用Formクラス.
  * 
@@ -12,10 +16,16 @@ public class InsertAdministratorForm {
 	
 	
 	/** 管理者氏名 */
+	@NotEmpty(message="氏名は必須です")
 	private String name;
 	/** 管理者メールアドレス */
+	@NotEmpty(message="メールアドレスは必須です")
+	@Email(message="メールアドレスの形式が不正です")
 	private String mailAddress;
 	/** 管理者パスワード */
+	@NotEmpty(message="パスワードは必須です")
+	@Pattern(regexp="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,16}$"
+	, message="半角英大文字、半角英小文字、半角数字をそれぞれ1文字以上使用し、8文字以上16文字以下で入力してください")
 	private String password;
 	
 	public String getName() {
