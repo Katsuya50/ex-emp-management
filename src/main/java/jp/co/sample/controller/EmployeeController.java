4c87343d39c6a23fb0d202ad90f986a2d3d9024b
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-05 17:38:04 +0900

    idに一致する従業員情報を取得して詳細画面にフォワードするメソッドを作成しました。

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -23,8 +25,13 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService service;
 	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
 	/**
-	 * 従業員リストを取得して従業員リスト画面にフォワードするメソッド.
+	 * 従業員リストをリクエストスコープに格納して従業員リスト画面にフォワードするメソッド.
 	 * 
 	 * @param model リクエストスコープ
 	 * @return 従業員リスト画面
@@ -34,6 +41,19 @@ public class EmployeeController {
 		List<Employee> employeeList = service.showList();
 		model.addAttribute("employeeList", employeeList);
 		return "/employee/list";
+	}
+	
+	/**
+	 * idに一致する従業員情報をリクエストスコープに格納して詳細画面にフォワードするメソッド.
+	 * 
+	 * @param id
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		model.addAttribute("employee", service.showDetail(Integer.parseInt(id)));
+		return "/employee/detail";
 	}
 
 }
