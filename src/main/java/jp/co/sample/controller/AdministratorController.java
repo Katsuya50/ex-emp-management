100bff00f16ec2be307636dceb4ce4bd4c15b81f
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-05 15:44:44 +0900

    ログイン処理のメソッドを作成しました。

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,9 +1,14 @@
 package jp.co.sample.controller;
 
+import java.util.Objects;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,6 +29,9 @@ public class AdministratorController {
 	
 	@Autowired
 	private AdministratorService service;
+	
+	@Autowired
+	private HttpSession session;
 	
 	@ModelAttribute
 	public InsertAdministratorForm setUpAdministratorForm() {
@@ -68,6 +76,26 @@ public class AdministratorController {
 	@RequestMapping("/")
 	public String toLogin() {
 		return "administrator/login";
+	}
+	
+	/**
+	 * 管理者がログインをするメソッド.
+	 * 
+	 * @param form ログイン画面から情報を受け取ったフォーム
+	 * @param resultOfErrorMessage ログイン失敗時に記載されるエラーメッセージ
+	 * @param model リクエストスコープ
+	 * @return ログイン失敗ならばログイン画面に戻る、成功ならば従業員リスト画面にフォワード
+	 */
+	@RequestMapping("/login")
+	public String login(LoginForm form, BindingResult resultOfErrorMessage, Model model) {
+		Administrator administrator = service.login(form.getMailAddress(), form.getPassword());
+		resultOfErrorMessage.resolveMessageCodes("メールアドレスまたはパスワードが不正です");
+		if(Objects.equals(administrator, null)) {
+			model.addAttribute("resultOfErrorMessage", resultOfErrorMessage);
+			return toLogin();
+		}
+		session.setAttribute("administratorName", administrator.getName());
+		return "/employee/showList";
 	}
 
 }
