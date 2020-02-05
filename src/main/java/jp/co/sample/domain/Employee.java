ea166fa30c12c2236f3eee80348e0e90f0a5f68f
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-05 10:57:53 +0900

    従業員ドメインにJavaDoc追加

--- src/main/java/jp/co/sample/domain/Employee.java
+++ src/main/java/jp/co/sample/domain/Employee.java
@@ -10,17 +10,29 @@ import java.util.Date;
  */
 public class Employee {
 	
+	/**	従業員ID */
 	private Integer id;
+	/**	従業員名 */
 	private String name;
+	/**	従業員画像 */
 	private String image;
+	/**	従業員性別 */
 	private String gender;
+	/**	従業員入社日 */
 	private Date hireDate; 
+	/**	従業員メールアドレス */
 	private String mailAddress;
+	/**	従業員郵便番号 */
 	private String zipCode;
+	/**	従業員住所 */
 	private String address;
+	/**	従業員電話番号 */
 	private String telephone;
+	/**	従業員給与 */
 	private Integer salary;
+	/**	従業員特性 */
 	private String characteristics;
+	/**	従業員扶養人数 */
 	private Integer dependentsCount;
 	
 	public Employee() {
