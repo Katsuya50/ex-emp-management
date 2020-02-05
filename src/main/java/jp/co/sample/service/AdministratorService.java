50be98c157c643dd28bfdaabf6d8fb089bc72a13
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-05 14:21:17 +0900

    管理者情報をテーブルに挿入するメソッドを作成しました。

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 /**
@@ -19,6 +20,13 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository repository;
 	
-	
+	/**
+	 * 管理者情報をテーブルに挿入する処理.
+	 * 
+	 * @param administrator
+	 */
+	public void insert(Administrator administrator) {
+		repository.insert(administrator);
+	}
 	
 }
