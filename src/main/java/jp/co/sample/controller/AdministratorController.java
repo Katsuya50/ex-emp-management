1f9a22f0f9a38dc777dfbef561cc51cdef470853
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-05 14:37:01 +0900

    管理者情報登録メソッドを作成しました。

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -34,6 +37,21 @@ public class AdministratorController {
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert.html";
+	}
+	
+	/**
+	 * 管理者情報登録メソッド.
+	 * administratorsテーブルに新しく行を挿入する
+	 * 
+	 * @param form 挿入する情報を受け取ったform
+	 * @return ログイン画面
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		service.insert(administrator);
+		return "redirect:/";
 	}
 
 }
