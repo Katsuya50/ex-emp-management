de9da9640d3a5828ed0fd3cd27aad01c452e1d76
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-05 13:53:50 +0900

    管理者情報の処理制御クラスを作成しました。

---
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -0,0 +1,39 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+/**
+ * 従業員情報の処理制御クラス.
+ * 
+ * @author blues
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+	
+	@Autowired
+	private AdministratorService service;
+	
+	@ModelAttribute
+	public InsertAdministratorForm setUpFormAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+	
+	/**
+	 * 管理者登録画面にフォワードするメソッド.
+	 * 
+	 * @return 管理者登録画面
+	 */
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert.html";
+	}
+
+}
