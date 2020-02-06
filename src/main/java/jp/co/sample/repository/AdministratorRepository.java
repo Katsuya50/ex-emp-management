1635504f53b2b3f0b0334fb052563ff6faaa4e66
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-06 11:03:48 +0900

    メールアドレス検索の並び替え削除

--- src/main/java/jp/co/sample/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -90,7 +90,7 @@ public class AdministratorRepository {
 	 * @return メールアドレスの情報のみを含んだ管理者リスト
 	 */
 	public List<Administrator> findAllOfMailAddress() {
-		String sql = "SELECT mail_address FROM " + TABLE_NAME + " ORDER BY id";
+		String sql = "SELECT mail_address FROM " + TABLE_NAME;
 		List<Administrator> AdministratorList = template.query(sql, ADMINISTRATOR_ROW_MAPPER);
 		return AdministratorList;
 	}
